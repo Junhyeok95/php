4a4fb1b3354f83b5c66f27a8b8fca0e6c5c74978
JANG JUNHYEOK <btrya23@gmail.com>
2020-07-12 23:14:14 +0900

    awsTest, artisan Tip

--- awsTest/routes/web.php
+++ awsTest/routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/json', function () {
+    return response()->json([
+        'name' => 'Jang',
+        'state' => 'KR'
+    ]);
+});
