9cd03bc3620c4769ecf2883bbac9a637f9e144bb
JANG JUNHYEOK <btrya23@gmail.com>
2020-08-25 21:21:52 +0900

    task_laravel, TDD .. TT ..

--- task-laravel/README.md
+++ task-laravel/README.md
@@ -6,4 +6,13 @@ create database task_laravel;
 php artisan migrate
 
 php artisan make:test AuthControllerTest
+php artisan make:controller AuthController
 ```
+
+###
+
+테스트 실행하는 법
+
+1. 커맨드라인 -> vendor/bin/phpunit tests
+2. 커맨드라인 -> vendor/bin/phpunit tests --filter [메소드 명]
+3. IDE의 단축키를 활용
--- task-laravel/tests/Feature/AuthControllerTest.php
+++ task-laravel/tests/Feature/AuthControllerTest.php
@@ -8,15 +8,32 @@ use Tests\TestCase;
 
 class AuthControllerTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function testExample()
-    {
-        $response = $this->get('/');
+    // 1. 테스트가 끝나면 지워버린다는 의미,
+    use RefreshDatabase, WithFaker;
 
-        $response->assertStatus(200);
+    // 2. test를 앞에 붙이거나 /** @test */ 를 앞에 달아야한다... 아마..?
+
+    /** @test */
+    public function a_user_can_be_registered()
+    {
+        // 에러 발생 시 헨들링
+        $this->withoutExceptionHandling();
+        $data = [
+            'first_name' => $this->faker->firstName, // WithFaker인 faker로 단어를 생성
+            'last_name' => $this->faker->lastName,
+            'email' => $this->faker->email,
+            'password' => 'password',
+            'password_confirmation' => 'password'
+        ];
+
+        $response = $this->post('/api/register', $data);
+
+        $response->assertStatus(201)->assertJsonStructure(['user']);
+
+        $this->assertDatabaseHas('users', [
+            'first_name' => $data['first_name'],
+            'last_name' => $data['last_name'],
+            'email' => $data['email'],
+        ]);
     }
 }
