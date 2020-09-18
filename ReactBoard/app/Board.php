c25a0e070d9cb1706956a2427716adf439405f5e
JANG JUNHYEOK <btrya23@gmail.com>
2020-09-18 23:18:24 +0900

    ReactBoard, BoardController2

--- ReactBoard/app/Board.php
+++ ReactBoard/app/Board.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Board extends Model
 {
-    //
+    protected $fillable = ['title', 'content'];
+
+    public function user()
+    {
+        // 외래 키를 별도로 설정하는 방법 : belongsTo(User::class, 'my_id');
+        return $this->belongsTo(User::class);
+    }
 }
