89dab096fbd48f0a2d78f457eae684cca79d2eb9
JANG JUNHYEOK <btrya23@gmail.com>
2020-09-24 23:01:59 +0900

    ReactBoard, TestPage2-2

--- ReactBoard/resources/js/pages/TestPage2/index.js
+++ ReactBoard/resources/js/pages/TestPage2/index.js
@@ -1,11 +1,12 @@
 import React, { Fragment, useState, useEffect } from "react";
 import styled from "styled-components";
-import { ListGroup } from "react-bootstrap";
+import { Table, Pagination } from "react-bootstrap";
 
 const StyledDiv = styled.div`
-  padding: 3rem;
+  padding: 1rem;
+  margin: 1rem;
   border: 1px black solid;
-  font-size: 72px;
+  font-size: 24px;
   background-color: white;
 `;
 
@@ -18,17 +19,94 @@ const TestPage2 = () => {
 
   return (
     <Fragment>
-      <div className="container py-4">
+      <div className="container py-1">
         <StyledDiv>{text}</StyledDiv>
       </div>
       <div>
-        <ListGroup>
-          <ListGroup.Item>Cras justo odio</ListGroup.Item>
-          <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-          <ListGroup.Item>Morbi leo risus</ListGroup.Item>
-          <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
-          <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
-        </ListGroup>
+        <StyledDiv>Tables</StyledDiv>
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Username</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+            </tr>
+            <tr>
+              <td>2</td>
+              <td>Jacob</td>
+              <td>Thornton</td>
+              <td>@fat</td>
+            </tr>
+            <tr>
+              <td>3</td>
+              <td colSpan="2">Larry the Bird</td>
+              <td>@twitter</td>
+            </tr>
+          </tbody>
+        </Table>
+      </div>
+      <div>
+        <StyledDiv>Small Table</StyledDiv>
+        <Table striped bordered hover size="sm">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Username</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+            </tr>
+            <tr>
+              <td>2</td>
+              <td>Jacob</td>
+              <td>Thornton</td>
+              <td>@fat</td>
+            </tr>
+            <tr>
+              <td>3</td>
+              <td colSpan="2">Larry the Bird</td>
+              <td>@twitter</td>
+            </tr>
+          </tbody>
+        </Table>
+      </div>
+      <div>
+        <StyledDiv>Pagination</StyledDiv>
+        <div style={{ backgroundColor: "red" }}>
+          <Pagination>
+            <Pagination.First />
+            <Pagination.Prev />
+            <Pagination.Item>{1}</Pagination.Item>
+            <Pagination.Ellipsis />
+
+            <Pagination.Item>{10}</Pagination.Item>
+            <Pagination.Item>{11}</Pagination.Item>
+            <Pagination.Item active>{12}</Pagination.Item>
+            <Pagination.Item>{13}</Pagination.Item>
+            <Pagination.Item disabled>{14}</Pagination.Item>
+
+            <Pagination.Ellipsis />
+            <Pagination.Item>{20}</Pagination.Item>
+            <Pagination.Next />
+            <Pagination.Last />
+          </Pagination>
+        </div>
       </div>
     </Fragment>
   );
